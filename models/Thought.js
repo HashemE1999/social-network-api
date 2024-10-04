645d3297f7999b3febb184990cb27634bb4d4790
Halcyon <hashem_elmaghrabi@yahoo.com>
2024-10-03 21:47:10 -0400

    Made changes to thought model, routes, and controller

--- models/Thought.js
+++ models/Thought.js
@@ -1,30 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // Import reactionSchema
-const reactionSchema = require('./Reaction');
+const reactionSchema = require("./Reaction");
 // Construct a new instance of the schema class
-const thoughtSchema = new mongoose.Schema({
+const thoughtSchema = new mongoose.Schema(
+  {
     // Configure individual properties using Schema Types
     thoughtText: {
       type: String,
       required: true,
       minLength: 1,
-      maxLength: 280
+      maxLength: 280,
     },
-    createdAt: { 
+    createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
       // format Date
     },
-    username: {
-        type: String,
-        required: true
+    thoughtname: {
+      type: String,
+      required: true,
     },
-    reactions: [
-      reactionSchema,
-    ]
-  });
-  
-  // Using mongoose.model() to compile a model based on the schema 'thoughtSchema'
-  const Thought = mongoose.model('Thought', thoughtSchema);
-  
-  module.exports = Thought;
+    reactions: [reactionSchema],
+  },
+  {
+    // Declaring virtual
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `reactionCount` that gets the amount of reactions per thought
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function () {
+    let reactions = this.reactions;
+    console.log(reactions)
+    return `${reactions.length}`;
+  })
+
+// Using mongoose.model() to compile a model based on the schema 'thoughtSchema'
+const Thought = mongoose.model("Thought", thoughtSchema);
+
+module.exports = Thought;
